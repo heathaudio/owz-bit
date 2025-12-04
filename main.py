35d562c752350763e30336f1a251f8133701da73
techdave84 <davidsheath@gmail.com>
2025-12-04 15:22:06 +0000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,35 +1,75 @@
-def on_pin_released_p0():
-    basic.show_string("" + str(score))
-input.on_pin_released(TouchPin.P0, on_pin_released_p0)
+def umpire():
+    global UDrolled, ball, score, wicket
+    UDrolled = umpiredie._pick_random()
+    basic.show_string(UDrolled)
+    if UDrolled == "not out":
+        ball += 1
+    elif UDrolled == "no ball":
+        score = noballpenalty + score
+    else:
+        ball += 1
+        wicket += 1
 
 def on_button_pressed_a():
-    global batrolled, owzthatrolled, wickets, score
-    batrolled = bat._pick_random()
-    basic.show_string("" + str(batrolled))
-    if batrolled == 0:
-        owzthatrolled = owzthat._pick_random()
-        basic.show_string("" + (owzthatrolled))
-        wickets += 1
-    else:
-        score = batrolled + score
+    bowl()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def gameendcheck():
+    global over, ball
+    if wicket == totalwicket:
+        basic.show_string("All out")
+        basic.show_string("" + str(score))
+    elif ball == ballover:
+        basic.show_string("End of Over " + ("" + str(over)))
+        over += 1
+        ball = 0
+        if over == totalover:
+            basic.show_string("End of Innings")
+            basic.show_string("" + str(score))
+def showscore():
+    basic.show_string("S" + ("" + str(score)) + "/" + ("" + str(wicket)))
+
+def on_button_pressed_ab():
+    basic.show_string("" + str(over))
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
 def on_button_pressed_b():
-    pass
+    showscore()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-wickets = 0
-owzthat: List[str] = []
-owzthatrolled = ""
-bat: List[number] = []
-batrolled = 0
+def bowl():
+    global BDrolled, score, ball
+    BDrolled = batterdie._pick_random()
+    basic.show_string("" + str(BDrolled))
+    if BDrolled != 0:
+        score = BDrolled + score
+        ball += 1
+    else:
+        umpire()
+    gameendcheck()
+batterdie: List[number] = []
+BDrolled = 0
+umpiredie: List[str] = []
+UDrolled = ""
+totalover = 0
+ballover = 0
+noballpenalty = 0
+totalwicket = 0
+wicket = 0
+over = 0
+ball = 0
 score = 0
 score = 0
-batrolled = 0
-score = 0
+ball = 0
+over = 1
+wicket = 0
+totalwicket = 10
+noballpenalty = 1
+ballover = 5
+totalover = 20
 
 def on_forever():
-    global bat, owzthat
-    bat = [0, 1, 2, 3, 4, 6]
-    owzthat = ["no out", "caught", "bowled", "no ball", "stumped", "lbw"]
+    global batterdie, umpiredie
+    batterdie = [0, 1, 2, 3, 4, 6]
+    umpiredie = ["not out", "caught", "bowled", "no ball", "stumped", "lbw"]
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,70 +1,72 @@
-input.onButtonPressed(Button.A, function () {
-    BDrolled = batterdie._pickRandom()
-    basic.showString("" + (BDrolled))
-    if (BDrolled != 0) {
-        score = BDrolled + score
+function umpire () {
+    UDrolled = umpiredie._pickRandom()
+    basic.showString(UDrolled)
+    if (UDrolled == "not out") {
         ball += 1
+    } else if (UDrolled == "no ball") {
+        score = noballpenalty + score
     } else {
-        UDrolled = umpiredie._pickRandom()
-        basic.showString(UDrolled)
-        serial.writeString(UDrolled)
-        if (UDrolled == "not out") {
-            ball += 1
-        } else if (UDrolled == "no ball") {
-            score = noballpenalty + score
-        } else {
-            ball += 1
-            wicket += 1
-        }
+        ball += 1
+        wicket += 1
     }
+}
+input.onButtonPressed(Button.A, function () {
+    bowl()
+})
+function gameendcheck () {
     if (wicket == totalwicket) {
         basic.showString("All out")
-        basic.showString("" + (score))
-        serial.writeLine("/All out/")
-        serial.writeString("" + (score))
+        basic.showString("" + score)
     } else if (ball == ballover) {
+        basic.showString("End of Over " + ("" + over))
         over += 1
         ball = 0
         if (over == totalover) {
             basic.showString("End of Innings")
-            basic.showString("" + (score))
-            serial.writeLine("/End of innings/")
-            serial.writeString("" + (score))
-        } else {
-        	
+            basic.showString("" + score)
         }
-    } else {
-    	
     }
-    serial.writeLine("/Ball ")
-    serial.writeString("" + (ball))
-    serial.writeLine("/Over ")
-    serial.writeString("" + (over))
-    serial.writeLine("/Score ")
-    serial.writeString("" + (score))
-    serial.writeLine("/Wickets ")
-    serial.writeString("" + (wicket))
+}
+function showscore () {
+    basic.showString("S" + ("" + score) + "/" + ("" + wicket))
+}
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + over)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("S" + score + "/" + wicket)
+    showscore()
 })
-let over = 0
-let wicket = 0
-let umpiredie: string[] = []
-let UDrolled = ""
-let ball = 0
+function bowl () {
+    BDrolled = batterdie._pickRandom()
+    basic.showString("" + BDrolled)
+    if (BDrolled != 0) {
+        score = BDrolled + score
+        ball += 1
+    } else {
+        umpire()
+    }
+    gameendcheck()
+}
 let batterdie: number[] = []
 let BDrolled = 0
-let totalwicket = 0
+let umpiredie: string[] = []
+let UDrolled = ""
 let totalover = 0
 let ballover = 0
 let noballpenalty = 0
+let totalwicket = 0
+let wicket = 0
+let over = 0
+let ball = 0
 let score = 0
 score = 0
+ball = 0
+over = 1
+wicket = 0
+totalwicket = 10
 noballpenalty = 1
 ballover = 5
 totalover = 20
-totalwicket = 10
 basic.forever(function () {
     batterdie = [
     0,
